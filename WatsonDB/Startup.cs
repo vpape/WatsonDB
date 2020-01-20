9dd37031fca90934b56064353ac13bd32c5d2c15
vpape <v.pape3@gmail.com>
2020-01-19 19:13:59 -0600

    Add project files.

---
+++ WatsonDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatsonDB.Startup))]
+namespace WatsonDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
